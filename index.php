6cb19b237379c6f534f32d74d113fd08e261b7f5
aysrg9 <egyditya@gmail.com>
2022-08-06 20:25:13 +0700

    responsive

--- index.php
+++ index.php
@@ -39,11 +39,11 @@
             <div class="container">
                 <a class="navbar-brand fs-2 text-primary" href="" style="font-family: 'Kanit', sans-serif;">Bukatoko</a>
                 <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"
-                        style="width: 400px;">
+                    <input class="input-search form-control me-2" type="search" placeholder="Search"
+                        aria-label="Search">
                     <button class="btn btn-outline-primary d-none" type="submit"><i class="bi bi-search"></i></button>
                 </form>
-                <div id="button">
+                <div id="button-navbar">
                     <a class="btn btn-primary fw-bold">LOGIN</a>
                     <a class="btn btn-primary fw-bold">REGISTER</a>
                 </div>
@@ -53,7 +53,7 @@
     <!-- End Navbar -->
 
     <!-- Carousel -->
-    <section id="carousel" style="padding-top: 150px; padding-bottom: 50px;">
+    <section id="carousel">
         <div class="container">
             <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
                 <div class="carousel-indicators">
@@ -66,16 +66,13 @@
                 </div>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="./assets/images/carousel/bukagames.png" width="100%" height="400px"
-                            class="d-block rounded" alt="...">
+                        <img src="./assets/images/carousel/bukagames.png" class="d-block rounded" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="./assets/images/carousel/bukareksa.png" width="100%" height="400px"
-                            class="d-block w-100 rounded" alt="...">
+                        <img src="./assets/images/carousel/bukareksa.png" class="d-block w-100 rounded" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="./assets/images/carousel/murahmantap.png" width="100%" height="400px"
-                            class="d-block w-100 rounded" alt="...">
+                        <img src="./assets/images/carousel/murahmantap.png" class="d-block w-100 rounded" alt="...">
                     </div>
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
@@ -98,13 +95,13 @@
         <div class="shadow" style="background-color: #ffffff;">
             <div class="pt-3 pb-4 ps-3 pe-3">
                 <h3 class="text-primary">For Egyditya</h3>
-                <div class="card mt-3 d-inline-block" style="width: 17rem;">
+                <div class="card-customer card mt-3 d-inline-block">
                     <div class="card-body bg-primary text-white rounded">
                         <h5 class="card-title">Cashback 50%</h5>
                         <p class="card-text">Check Bukatoko Birthday Surprise for your first purchase.</p>
                     </div>
                 </div>
-                <div class="card mt-3 d-inline-block ms-2" style="width: 17rem;">
+                <div class="card-customer card mt-3 d-inline-block">
                     <div class="card-body bg-primary text-white rounded">
                         <h5 class="card-title">Free Shipping</h5>
                         <p class="card-text">Free shipping for the island of Java.</p>
@@ -120,36 +117,116 @@
         <div class="shadow" style="background-color: #ffffff;">
             <h3 class="pt-2 pb-2 text-center text-primary">Recommendation Product</h3>
         </div>
-        <div class="row row-cols-2 row-cols-lg-6 g-2 g-lg-3 mt-3">
+        <div class="row row-cols-2 row-cols-lg-5 g-2 g-sm-2 mt-3">
             <div class="col">
-                <div class="p-3 shadow-sm bg-white">Row column</div>
+                <div class="p-3 shadow-sm bg-white">
+                    <div style="width: auto;">
+                        <img src="./assets/images/product/62bb3f4a6be9a.png" class="card-img-top" alt="...">
+                        <div class="card-body pt-3">
+                            <p class="card-title text-truncate ">Prosesor Intel® Core™ i3-12100F</p>
+                            <p class="card-title pt-2 fw-bold">Rp 1.399.000</p>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col">
-                <div class="p-3 shadow-sm bg-white">Row column</div>
+                <div class="p-3 shadow-sm bg-white">
+                    <div style="width: auto;">
+                        <img src="./assets/images/product/62bb3f4a6be9a.png" class="card-img-top" alt="...">
+                        <div class="card-body pt-3">
+                            <p class="card-title text-truncate ">Prosesor Intel® Core™ i3-12100F</p>
+                            <p class="card-title pt-2 fw-bold">Rp 1.399.000</p>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col">
-                <div class="p-3 shadow-sm bg-white">Row column</div>
+                <div class="p-3 shadow-sm bg-white">
+                    <div style="width: auto;">
+                        <img src="./assets/images/product/62bb3f4a6be9a.png" class="card-img-top" alt="...">
+                        <div class="card-body pt-3">
+                            <p class="card-title text-truncate ">Prosesor Intel® Core™ i3-12100F</p>
+                            <p class="card-title pt-2 fw-bold">Rp 1.399.000</p>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col">
-                <div class="p-3 shadow-sm bg-white">Row column</div>
+                <div class="p-3 shadow-sm bg-white">
+                    <div style="width: auto;">
+                        <img src="./assets/images/product/62bb3f4a6be9a.png" class="card-img-top" alt="...">
+                        <div class="card-body pt-3">
+                            <p class="card-title text-truncate ">Prosesor Intel® Core™ i3-12100F</p>
+                            <p class="card-title pt-2 fw-bold">Rp 1.399.000</p>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col">
-                <div class="p-3 shadow-sm bg-white">Row column</div>
+                <div class="p-3 shadow-sm bg-white">
+                    <div style="width: auto;">
+                        <img src="./assets/images/product/62bb3f4a6be9a.png" class="card-img-top" alt="...">
+                        <div class="card-body pt-3">
+                            <p class="card-title text-truncate ">Prosesor Intel® Core™ i3-12100F</p>
+                            <p class="card-title pt-2 fw-bold">Rp 1.399.000</p>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col">
-                <div class="p-3 shadow-sm bg-white">Row column</div>
+                <div class="p-3 shadow-sm bg-white">
+                    <div style="width: auto;">
+                        <img src="./assets/images/product/62bb3f4a6be9a.png" class="card-img-top" alt="...">
+                        <div class="card-body pt-3">
+                            <p class="card-title text-truncate ">Prosesor Intel® Core™ i3-12100F</p>
+                            <p class="card-title pt-2 fw-bold">Rp 1.399.000</p>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col">
-                <div class="p-3 shadow-sm bg-white">Row column</div>
+                <div class="p-3 shadow-sm bg-white">
+                    <div style="width: auto;">
+                        <img src="./assets/images/product/62bb3f4a6be9a.png" class="card-img-top" alt="...">
+                        <div class="card-body pt-3">
+                            <p class="card-title text-truncate ">Prosesor Intel® Core™ i3-12100F</p>
+                            <p class="card-title pt-2 fw-bold">Rp 1.399.000</p>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col">
-                <div class="p-3 shadow-sm bg-white">Row column</div>
+                <div class="p-3 shadow-sm bg-white">
+                    <div style="width: auto;">
+                        <img src="./assets/images/product/62bb3f4a6be9a.png" class="card-img-top" alt="...">
+                        <div class="card-body pt-3">
+                            <p class="card-title text-truncate ">Prosesor Intel® Core™ i3-12100F</p>
+                            <p class="card-title pt-2 fw-bold">Rp 1.399.000</p>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col">
-                <div class="p-3 shadow-sm bg-white">Row column</div>
+                <div class="p-3 shadow-sm bg-white">
+                    <div style="width: auto;">
+                        <img src="./assets/images/product/62bb3f4a6be9a.png" class="card-img-top" alt="...">
+                        <div class="card-body pt-3">
+                            <p class="card-title text-truncate ">Prosesor Intel® Core™ i3-12100F</p>
+                            <p class="card-title pt-2 fw-bold">Rp 1.399.000</p>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col">
-                <div class="p-3 shadow-sm bg-white">Row column</div>
+                <div class="p-3 shadow-sm bg-white">
+                    <div style="width: auto;">
+                        <img src="./assets/images/product/62bb3f4a6be9a.png" class="card-img-top" alt="...">
+                        <div class="card-body pt-3">
+                            <p class="card-title text-truncate">Prosesor Intel® Core™ i3-12100F</p>
+                            <p class="card-title pt-2 fw-bold">Rp 1.399.000</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
